3f664b6d62fcfd0c8794ec64f75dd9e59c9c9850
junjungoal <junyamada107@gmail.com>
2022-12-24 16:21:41 +0000

    modify scripts

--- shapestacks_creator/create_stack_scenario.sh
+++ shapestacks_creator/create_stack_scenario.sh
@@ -18,7 +18,7 @@ create_params()
 {
   v=$1
   template_path=${SHAPESTACKS_CODE_HOME}/data/shapestacks_example/mjcf   # $1: model_path
-  export_path=${SHAPESTACKS_CODE_HOME}/data/shapestacks_example/mjcf  # $2: record_path
+  export_path=${SHAPESTACKS_CODE_HOME}/data/shapestacks_example/mjcf_stack  # $2: record_path
 
   # randomize light and textures
   height=`python -c "import random; print(random.randint(3,${MAX_HEIGHT}))"`
